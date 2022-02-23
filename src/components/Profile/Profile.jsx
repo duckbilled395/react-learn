bf84d3d4d6fb1990a02c207eb571d25f8b4e0c4e
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-02-23 16:07:47 +0300

    update

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import MyPosts from "./MyPosts/MyPosts";
+import styles from'./Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://archzine.fr/wp-content/uploads/2016/03/le-belle-photo-image-la-plus-belle-image-du-monde.jpg' alt='pic'></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <MyPosts />
+        </div>
+    );
+}
+
+export default Profile;
