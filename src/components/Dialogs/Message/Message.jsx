1bea252e5e78500937607bb050aa513a14d1a52d
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-02-28 23:14:12 +0300

    123

--- src/components/Dialogs/Message/Message.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from './Message.module.css';
-
-const Message = (props) => {
-    return <div className={props.abv}>{props.message}</div>
-}
-
-export default Message;
