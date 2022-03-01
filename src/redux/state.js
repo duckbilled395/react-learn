61005c971e7f8b0cf02d168f87cb533d4eaa1536
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-03-01 16:14:08 +0300

    123123

---
+++ src/redux/state.js
@@ -0,0 +1,73 @@
+let rerenderEntireTree = () => {
+  console.log(123);
+}
+
+let state = {
+  profilePage: {
+    postsData: [
+        {id: 2, message: 'Hi, how are you?', likesCount: 12},
+        {id: 1, message: 'It\'s my first post', likesCount: 125},
+        {id: 3, message: 'Yo', likesCount: 11},
+        {id: 4, message: 'Yo', likesCount: 16},
+        {id: 5, message: 'Yo', likesCount: 12},
+        {id: 6, message: 'Yo', likesCount: 1}
+    ],
+    newPostText: 'it-kamasutra'
+  },
+  messagesPage: {
+    messagesData: [
+        {id: 2, message: 'Hi'},
+        {id: 1, message: 'How are you?'},
+        {id: 3, message: 'YoYO'},
+        {id: 4, message: 'YoYO'},
+        {id: 5, message: 'Yo'},
+        {id: 6, message: 'Yo'}
+    ],
+    dialogsData: [
+      {id: 1, name: 'Victor', avatar: 'https://bipbap.ru/wp-content/uploads/2017/08/04.-risunki-dlya-srisovki-legkie-dlya-devochek.jpg'},
+      {id: 2, name: 'Sveta', avatar: 'https://miro.medium.com/max/3150/1*C-34yLiaL1xakwUHROTWwQ.png'},
+      {id: 3, name: 'Vanya', avatar: 'https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg'},
+      {id: 4, name: 'Andrey', avatar: 'https://cdn2.unrealengine.com/18br-headband-cosmetics-naruto-toast-400x400-5228564ee685.jpg'},
+      {id: 5, name: 'Vika', avatar: 'https://a.d-cd.net/2bd7a44s-960.jpg'},
+      {id: 6, name: 'Roma', avatar: 'https://zvukogram.com/upload/cimg-1-1605564625.jpg'}
+    ]
+  },
+  // sideBar: {
+  //   friendsList: [
+  //     {id: 1, name: 'Victor', avatar: 'https://bipbap.ru/wp-content/uploads/2017/08/04.-risunki-dlya-srisovki-legkie-dlya-devochek.jpg'},
+  //     {id: 2, name: 'Sveta', avatar: 'https://miro.medium.com/max/3150/1*C-34yLiaL1xakwUHROTWwQ.png'},
+  //     {id: 3, name: 'Vanya', avatar: 'https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg'},
+  //     {id: 4, name: 'Andrey', avatar: 'https://cdn2.unrealengine.com/18br-headband-cosmetics-naruto-toast-400x400-5228564ee685.jpg'},
+  //     {id: 5, name: 'Vika', avatar: 'https://a.d-cd.net/2bd7a44s-960.jpg'},
+  //     {id: 6, name: 'Roma', avatar: 'https://zvukogram.com/upload/cimg-1-1605564625.jpg'}
+  //   ]
+  // }  
+}
+
+// window.state = state;
+
+
+export const addPost = () => {
+  let newPost = {
+    id: state.profilePage.postsData.length + 1,
+    message: state.profilePage.newPostText,
+    likesCount: 0
+  };
+
+  state.profilePage.postsData.push(newPost);
+  console.log(state.profilePage)
+  state.profilePage.newPostText = '';
+  rerenderEntireTree(state);
+}
+
+export const updateNewPostText = (newText) => {
+
+  state.profilePage.newPostText = newText;
+  rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
+}
+
+export default state;
