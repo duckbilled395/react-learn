237f4754877cf30dcf495516df0487cb21784b05
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-03-11 16:03:34 +0300

    feat: users

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./Users.module.css";
+
+const Users = (props) => {
+
+    if (props.users.length === 0) {
+        props.setUsers([
+            {
+                id: 1,
+                fullName: 'Dmitry',
+                status: 'I am a boss',
+                location: {city: 'Minsk', country: 'Belarus'},
+                followed: false,
+                photoUrl: 'https://bipbap.ru/wp-content/uploads/2017/08/04.-risunki-dlya-srisovki-legkie-dlya-devochek.jpg'
+            },
+            {
+                id: 2,
+                fullName: 'Sasha',
+                status: 'I am a boss too',
+                location: {city: 'Moscow', country: 'Russia'},
+                followed: false,
+                photoUrl: 'https://bipbap.ru/wp-content/uploads/2017/08/04.-risunki-dlya-srisovki-legkie-dlya-devochek.jpg'
+            },
+            {
+                id: 3,
+                fullName: 'Andrey',
+                status: 'I am a boss too',
+                location: {city: 'Kiev', country: 'Ukraine'},
+                followed: true,
+                photoUrl: 'https://bipbap.ru/wp-content/uploads/2017/08/04.-risunki-dlya-srisovki-legkie-dlya-devochek.jpg'
+            }
+        ])
+    }
+    return (
+        <div>
+            {
+                props.users.map(u => <div key={u.id}>
+                    <span>
+                        <div>
+                            <img src={u.photoUrl} className={styles.userPhoto} alt="users photo"/>
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>
+                            }
+
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.fullName}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{u.location.country}</div>
+                            <div>{u.location.city}</div>
+                        </span>
+                    </span>
+                </div>)
+            }
+        </div>
+    )
+}
+
+export default Users;
