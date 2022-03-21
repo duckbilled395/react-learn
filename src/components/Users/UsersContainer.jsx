fd9df82ab89066699afa1db07ab714e7f2bb9e1c
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-03-21 16:00:29 +0300

    zxczxczxc

---
+++ src/components/Users/UsersContainer.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import {connect} from "react-redux";
+import {
+    follow,
+    setCurrentPage,
+    unfollow,
+    toggleFollowingProgress,
+    getUsers
+} from "../../redux/users-reducer";
+import Users from "./Users";
+import Preloader from "../common/Preloader/Preloader";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {compose} from "redux";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.getUsers(this.props.currentPage, this.props.pageSize);
+    }
+
+    onPageChanged = (pageNumber) => {
+
+        this.props.getUsers(pageNumber, this.props.pageSize, pageNumber);
+
+        // this.props.setCurrentPage(pageNumber);
+        // this.props.toggleIsFetching(true);
+        //
+        // usersAPI.getUsers(pageNumber, this.props.pageSize)
+        //     .then(data => {
+        //         this.props.toggleIsFetching(false);
+        //         this.props.setUsers(data.items);
+        //     })
+    }
+
+    render() {
+        console.log(this.props)
+        return <>
+            {this.props.isFetching
+                ? <Preloader />
+                : <Users totalUsersCount={this.props.totalUsersCount}
+                         pageSize={this.props.pageSize}
+                         currentPage={this.props.currentPage}
+                         onPageChanged={this.onPageChanged}
+                         users={this.props.users}
+                         follow={this.props.follow}
+                         unfollow={this.props.unfollow}
+                         followingInProgress={this.props.followingInProgress}
+                />}
+        </>
+    }
+}
+
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
+    }
+}
+
+// let mapDispatchToProps = (dispatch) => {
+//     return {
+//         follow: (userId) => {
+//             dispatch(followAC(userId));
+//         },
+//         unfollow: (userId) => {
+//             dispatch(unfollowAC(userId));
+//         },
+//         setUsers: (users) => {
+//             dispatch(setUsersAC(users));
+//         },
+//         setCurrentPage: (pageNumber) => {
+//             dispatch(setCurrentPageAc(pageNumber));
+//         },
+//         setTotalUsersCount: (totalCount) => {
+//             dispatch(setUsersTotalCountAC(totalCount));
+//         },
+//         toggleIsFetching: (isFetching) => {
+//             dispatch(toggleIsFetchingAC(isFetching));
+//         }
+//     }
+// }
+
+export default compose(
+    withAuthRedirect,
+    connect(mapStateToProps, {
+        follow,
+        unfollow,
+        setCurrentPage,
+        toggleFollowingProgress,
+        getUsers})
+)(UsersContainer)
---
+++ src/redux/redux-store.js
@@ -0,0 +1,21 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import dialogsPageReducer from "./dialogs-page-reducer";
+import profilePageReducer from "./profile-page-reducer";
+import sideBarReducer from "./side-bar-reducer";
+import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
+import thunkMiddleware  from 'redux-thunk';
+
+let reducers = combineReducers({
+    profilePage: profilePageReducer,
+    dialogsPage: dialogsPageReducer,
+    sideBar: sideBarReducer,
+    usersPage: usersReducer,
+    auth: authReducer
+});
+
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+// console.log(store.getState())
+export default store;
+
+window.store = store;
