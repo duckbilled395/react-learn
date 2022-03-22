e0ee3a723a14576a78a25db0f7cdc0ff5b79a99f
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-03-22 15:54:33 +0300

    zxczxczxc

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./Users.module.css";
+import userPhoto from "../../assets/images/user.png";
+import {NavLink} from "react-router-dom";
+
+let Users = (props) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        if (pages.length < 10) {
+            pages.push(i);
+        }
+    }
+
+    return (<div>
+        <div>
+            {pages.map(page => {
+                return <span
+                    className={props.currentPage === page
+                        ? styles.selectedPage
+                        : styles.page}
+                    onClick={(e) => {props.onPageChanged(page)}}
+                    key={page}>{page}
+                </span>
+            })}
+        </div>
+        {props.users.map(user => <div key={user.id}>
+                    <span>
+                        <div>
+                            <NavLink to={'/profile/' + user.id}>
+                                <img src={user.photos.small != null
+                                    ? user.photos.small
+                                    : userPhoto}
+                                     className={styles.userPhoto}
+                                 alt="users avatar"/>
+                            </NavLink>
+                        </div>
+                        <div>
+                            {user.followed
+                                ? <button disabled={props.followingInProgress.some(id => id === user.id)}
+                                          onClick={() => {props.unfollow(user.id)}}>Unfollow</button>
+                                : <button disabled={props.followingInProgress.some(id => id === user.id)}
+                                          onClick={() => {props.follow(user.id)}}>Follow</button>}
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{user.name}</div>
+                            <div>{user.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
+                    </span>
+        </div>)}
+    </div>)
+}
+
+export default Users;
