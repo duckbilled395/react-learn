17062968f7c85076f2f82791df6712b616dda107
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-03-24 22:50:50 +0300

    123

--- src/components/common/Preloader/Preloader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import preloader from "../../../assets/images/preloader.gif";
-
-const Preloader = (props) => {
-    return <div><img src={preloader} alt="loader" /></div>
-}
-
-export default Preloader;
